c08fb2562467e55754cb29bea6bef04fa3b56295
Nakul Bajaj <bajaj.nakul@gmail.com>
2021-10-03 17:13:57 +1100

    Removed the extra c command from download kaggle command

--- deployHouseRegressionModel/scripts/fetch_kaggle_dataset.sh
+++ deployHouseRegressionModel/scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kaggle competitions download -c -c house-prices-advanced-regression-techniques -p datasets/ 
+kaggle competitions download -c house-prices-advanced-regression-techniques -p datasets/ 
