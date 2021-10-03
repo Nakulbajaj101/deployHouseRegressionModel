a7019f4b9c2ecd47ecee2bb0da9ebaaa60245fa9
Nakul Bajaj <bajaj.nakul@gmail.com>
2021-10-04 00:23:11 +1100

    Added a script to publish model to gemfury

---
+++ deployHouseRegressionModel/housePriceModelling/publish_model.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+python -m pip install --upgrade build
+python -m build
+
+for pkg in $(ls dist)
+do 
+    echo "Pushing package $pkg to gemfury"
+    curl -F package=@"dist/$pkg" $GEMFURY_PUSH_URL
+done
